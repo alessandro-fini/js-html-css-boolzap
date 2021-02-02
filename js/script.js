b6ab5ccfe008745d0d5dbf628fea30c8110f7abd
alessandro <finialessandro90@gmail.com>
2021-02-02 14:17:59 +0100

    messaggi dall'array

--- js/script.js
+++ js/script.js
@@ -10,42 +10,67 @@ var app = new Vue ({
       {
         avatar: 'img/avatar_1.jpg',
         imgAlt:'avatar',
-        name: 'Michele'
+        name: 'Michele',
+        visible: false
       },
       {
         avatar: 'img/avatar_2.jpg',
         imgAlt:'avatar',
-        name: 'Fabio'
+        name: 'Fabio',
+        visible: false
       },
       {
         avatar: 'img/avatar_3.jpg',
         imgAlt:'avatar',
-        name: 'Samuele'
+        name: 'Samuele',
+        visible: false,
+        messages: [
+          {
+          date: '28/03/2020 10:10:40',
+          text: 'La Marianna va in campagna',
+          status: 'received'
+          },
+          {
+          date: '28/03/2020 10:20:10',
+          text: 'Sicuro di non aver sbagliato chat?',
+          status: 'sent'
+          },
+          {
+          date: '28/03/2020 16:15:22',
+          text: 'Ah scusa!',
+          status: 'received'
+          }
+        ]
       },
       {
         avatar: 'img/avatar_4.jpg',
         imgAlt:'avatar',
-        name: 'Luis'
+        name: 'Luis',
+        visible: false
       },
       {
         avatar: 'img/avatar_5.jpg',
         imgAlt:'avatar',
-        name: 'Paolo'
+        name: 'Paolo',
+        visible: false
       },
       {
         avatar: 'img/avatar_6.jpg',
         imgAlt:'avatar',
-        name: 'Martina'
+        name: 'Martina',
+        visible: false
       },
       {
         avatar: 'img/avatar_7.jpg',
         imgAlt:'avatar',
-        name: 'Aldo'
+        name: 'Aldo',
+        visible: false
       },
       {
         avatar: 'img/avatar_8.jpg',
         imgAlt:'avatar',
-        name: 'Ludovico'
+        name: 'Ludovico',
+        visible: false
       }
     ]
   }
