c93ffcf7198ed441c3ff7169627fc280a5b5be7a
alessandro <finialessandro90@gmail.com>
2021-02-02 10:52:19 +0100

    lista contatti

--- js/script.js
+++ js/script.js
@@ -6,5 +6,47 @@ var app = new Vue ({
       imgAlt: 'avatar',
       name: 'Jill Porter'
     },
+    contacts: [
+      {
+        avatar: 'img/avatar_1.jpg',
+        imgAlt:'avatar',
+        name: 'Michele'
+      },
+      {
+        avatar: 'img/avatar_2.jpg',
+        imgAlt:'avatar',
+        name: 'Fabio'
+      },
+      {
+        avatar: 'img/avatar_3.jpg',
+        imgAlt:'avatar',
+        name: 'Samuele'
+      },
+      {
+        avatar: 'img/avatar_4.jpg',
+        imgAlt:'avatar',
+        name: 'Luis'
+      },
+      {
+        avatar: 'img/avatar_5.jpg',
+        imgAlt:'avatar',
+        name: 'Paolo'
+      },
+      {
+        avatar: 'img/avatar_6.jpg',
+        imgAlt:'avatar',
+        name: 'Martina'
+      },
+      {
+        avatar: 'img/avatar_7.jpg',
+        imgAlt:'avatar',
+        name: 'Aldo'
+      },
+      {
+        avatar: 'img/avatar_8.jpg',
+        imgAlt:'avatar',
+        name: 'Ludovico'
+      }
+    ]
   }
 });
