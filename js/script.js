8e9fa74c1fa863af6e761284a79b2623a837a515
alessandro <finialessandro90@gmail.com>
2021-02-03 10:30:46 +0100

    visualizzazione chat user selezionato

--- js/script.js
+++ js/script.js
@@ -2,28 +2,58 @@ var app = new Vue ({
   el: '#app',
   data: {
     myUser: {
-      avatar: 'img/avatar_io.jpg',
-      imgAlt: 'avatar',
-      name: 'Jill Porter'
+      avatar: '_8',
+      name: 'Alessandro'
     },
     contacts: [
       {
-        avatar: 'img/avatar_1.jpg',
-        imgAlt:'avatar',
+        avatar: '_1',
         name: 'Michele',
-        visible: false
+        visible: true,
+        messages: [
+          {
+          date: '10/01/2020 15:30:55',
+          text: 'Hai portato a spasso il cane?',
+          status: 'sent'
+          },
+          {
+          date: '10/01/2020 15:50:00',
+          text: 'Ricordati di dargli da mangiare',
+          status: 'sent'
+          },
+          {
+          date: '10/01/2020 16:15:22',
+          text: 'Tutto fatto!',
+          status: 'received'
+          }
+        ]
       },
       {
-        avatar: 'img/avatar_2.jpg',
-        imgAlt:'avatar',
+        avatar: '_2',
         name: 'Fabio',
-        visible: false
+        visible: true,
+        messages: [
+          {
+          date: '20/03/2020 16:30:00',
+          text: 'Ciao come stai?',
+          status: 'sent'
+          },
+          {
+          date: '20/03/2020 16:30:55',
+          text: 'Bene grazie! Stasera ci vediamo?',
+          status: 'received'
+          },
+          {
+          date: '20/03/2020 16:35:00',
+          text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+          status: 'sent'
+          }
+        ]
       },
       {
-        avatar: 'img/avatar_3.jpg',
-        imgAlt:'avatar',
+        avatar: '_3',
         name: 'Samuele',
-        visible: false,
+        visible: true,
         messages: [
           {
           date: '28/03/2020 10:10:40',
@@ -43,35 +73,144 @@ var app = new Vue ({
         ]
       },
       {
-        avatar: 'img/avatar_4.jpg',
-        imgAlt:'avatar',
+        avatar: '_4',
         name: 'Luis',
-        visible: false
+        visible: true,
+        messages: [
+          {
+          date: '10/01/2020 15:30:55',
+          text: 'Hai portato a spasso il cane?',
+          status: 'sent'
+          },
+          {
+          date: '10/01/2020 15:50:00',
+          text: 'Ricordati di dargli da mangiare',
+          status: 'sent'
+          },
+          {
+          date: '10/01/2020 16:15:22',
+          text: 'Tutto fatto!',
+          status: 'received'
+          }
+        ]
       },
       {
-        avatar: 'img/avatar_5.jpg',
-        imgAlt:'avatar',
+        avatar: '_5',
         name: 'Paolo',
-        visible: false
+        visible: true,
+        messages: [
+          {
+          date: '28/03/2020 10:10:40',
+          text: 'La Marianna va in campagna',
+          status: 'received'
+          },
+          {
+          date: '28/03/2020 10:20:10',
+          text: 'Sicuro di non aver sbagliato chat?',
+          status: 'sent'
+          },
+          {
+          date: '28/03/2020 16:15:22',
+          text: 'Ah scusa!',
+          status: 'received'
+          }
+        ]
       },
       {
-        avatar: 'img/avatar_6.jpg',
-        imgAlt:'avatar',
+        avatar: '_6',
         name: 'Martina',
-        visible: false
+        visible: true,
+        messages: [
+          {
+          date: '10/01/2020 15:30:55',
+          text: 'Hai portato a spasso il cane?',
+          status: 'sent'
+          },
+          {
+          date: '10/01/2020 15:50:00',
+          text: 'Ricordati di dargli da mangiare',
+          status: 'sent'
+          },
+          {
+          date: '10/01/2020 16:15:22',
+          text: 'Tutto fatto!',
+          status: 'received'
+          }
+        ]
       },
       {
-        avatar: 'img/avatar_7.jpg',
-        imgAlt:'avatar',
+        avatar: '_7',
         name: 'Aldo',
-        visible: false
+        visible: true,
+        messages: [
+          {
+          date: '20/03/2020 16:30:00',
+          text: 'Ciao come stai?',
+          status: 'sent'
+          },
+          {
+          date: '20/03/2020 16:30:55',
+          text: 'Bene grazie! Stasera ci vediamo?',
+          status: 'received'
+          },
+          {
+          date: '20/03/2020 16:35:00',
+          text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+          status: 'sent'
+          }
+        ]
       },
       {
-        avatar: 'img/avatar_8.jpg',
-        imgAlt:'avatar',
-        name: 'Ludovico',
-        visible: false
+        avatar: '_io',
+        name: 'Elena',
+        visible: true,
+        messages: [
+          {
+          date: '10/01/2020 15:30:55',
+          text: 'Hai portato a spasso il cane?',
+          status: 'sent'
+          },
+          {
+          date: '10/01/2020 15:50:00',
+          text: 'Ricordati di dargli da mangiare',
+          status: 'sent'
+          },
+          {
+          date: '10/01/2020 16:15:22',
+          text: 'Tutto fatto!',
+          status: 'received'
+          }
+        ]
       }
-    ]
+    ],
+    activeContact: {
+      avatar: '_1',
+      name: 'Michele',
+      visible: true,
+      messages: [
+        {
+        date: '10/01/2020 15:30:55',
+        text: 'Hai portato a spasso il cane?',
+        status: 'sent'
+        },
+        {
+        date: '10/01/2020 15:50:00',
+        text: 'Ricordati di dargli da mangiare',
+        status: 'sent'
+        },
+        {
+        date: '10/01/2020 16:15:22',
+        text: 'Tutto fatto!',
+        status: 'received'
+        }
+      ]
+    }
+  },
+  methods: {
+    /* sostituzione del contatto attivo */
+    toActive: function(index){
+      this.activeContact = this.contacts[index];
+      console.log(this.activeContact);
+    }
   }
 });
